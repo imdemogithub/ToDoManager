bcf55d9ab6851ae3c162c82b691c7bd86eba39be
imdemogithub <87413356+imdemogithub@users.noreply.github.com>
2022-05-26 15:27:43 +0530

    updated

--- ToDoNotes/settings.py
+++ ToDoNotes/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
